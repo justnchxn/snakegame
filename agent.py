2cdddd7ba81296fbca0b3e8d67121482b2b1cab3
Justin Chan <justnchxn@gmail.com>
2024-03-28 02:31:16 +0000

    fixing errors

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 from collections import deque
 from game import SnakeGame, Direction, Point
+from model import Linear_QNet, QTrainer
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -13,11 +14,10 @@ class Agent:
     def __init__(self):
         self.n_games = 0
         self.epsilon = 0 # randomness
-        self.gamma = 0 # discount rate
+        self.gamma = 0.9 # discount rate (must be less than 1)
         self.memory = deque(maxlen=MAX_MEMORY) #popleft()
-        self.model = None
-        self.trainer = None
-        # model, trainer
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, learning_rate=LEARNING_RATE, gamma=self.gamma)
 
     def get_state(self, game):
         head = game.snake[0]
@@ -33,22 +33,22 @@ class Agent:
 
         state = [
             # Danger straight
-            (dir_r and game.is_collision(point_r)) or 
-            (dir_l and game.is_collision(point_l)) or 
-            (dir_u and game.is_collision(point_u)) or 
-            (dir_d and game.is_collision(point_d)),
+            (dir_r and game.check_collision(point_r)) or 
+            (dir_l and game.check_collision(point_l)) or 
+            (dir_u and game.check_collision(point_u)) or 
+            (dir_d and game.check_collision(point_d)),
 
             # Danger right
-            (dir_u and game.is_collision(point_r)) or 
-            (dir_d and game.is_collision(point_l)) or 
-            (dir_l and game.is_collision(point_u)) or 
-            (dir_r and game.is_collision(point_d)),
+            (dir_u and game.check_collision(point_r)) or 
+            (dir_d and game.check_collision(point_l)) or 
+            (dir_l and game.check_ollision(point_u)) or 
+            (dir_r and game.check_collision(point_d)),
 
             # Danger left
-            (dir_d and game.is_collision(point_r)) or 
-            (dir_u and game.is_collision(point_l)) or 
-            (dir_r and game.is_collision(point_u)) or 
-            (dir_l and game.is_collision(point_d)),
+            (dir_d and game.check_collision(point_r)) or 
+            (dir_u and game.check_collision(point_l)) or 
+            (dir_r and game.check_collision(point_u)) or 
+            (dir_l and game.check_collision(point_d)),
             
             # Move direction
             dir_l,
@@ -90,7 +90,7 @@ class Agent:
             final_move[move] = 1
         else:
             state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.model.predict(state0)
+            prediction = self.model(state0)
             move = torch.argmax(prediction).item()
             final_move[move] = 1
         return final_move
@@ -109,7 +109,7 @@ def train():
         final_move = agent.get_action(state_old)
 
         # perform move and get new state
-        reward, done, score = game.play_step(final_move)
+        reward, done, score = game.game_step(final_move)
         state_new = agent.get_state(game)
 
         # train short memory
@@ -129,6 +129,13 @@ def train():
                 agent.model.save()
 
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+
 
 if __name__ == '__main__':
     train()
--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import os
 
 class Linear_QNet(nn.Module): 
     def __init__(self, input_size, hidden_size, output_size):
-        super():__init__()
+        super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(input_size, output_size)
 
@@ -21,4 +21,41 @@ class Linear_QNet(nn.Module):
             os.makedirs(model_folder_path)
 
         file_name = os.path.join(model_folder_path, file_name)
-        torch.save(self.state_dict())
+        torch.save(self.state_dict(), file_name)
+
+class QTrainer:
+    def __init__(self, model, learning_rate, gamma):
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), learning_rate)
+        self.criterion = nn.MSELoss()
+
+    def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(state, dtype=torch.float)
+        action = torch.tensor(state, dtype=torch.float)
+        reward = torch.tensor(state, dtype=torch.float)
+
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+        pred = self.model(state)
+
+        target = pred.clone()
+        for idx in range(len(done)):
+            Q_new = reward(idx)
+            if not done(idx):
+                Q_new = reward(idx) + self.gamma + torch.max(self.model(next_state[idx]))
+            
+            target[idx] [torch.argmax(action).item()] = Q_new
+
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+
+        self.optimizer.step()
